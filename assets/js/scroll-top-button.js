a48146e5995e6e27d6ab6709acb3bf1d71ff34a8
Diana Stepanova <diana@MacBook-Air-Diana.local>
2025-04-15 13:28:06 +0500

    wip

--- assets/js/scroll-top-button.js
+++ assets/js/scroll-top-button.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const windowH = window.innerHeight;
     const docH    = document.documentElement.scrollHeight;
 
-    // Если пользователь прокрутил до 95% страницы
     if (scrollY + windowH >= docH * 0.95) {
       btn.classList.add('visible');
     } else {
